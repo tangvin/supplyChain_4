dafbba70eca35c7f3705cddd709568e376f8deae
yuehansheng <1053258019@qq.com>
2017-12-07 21:54:23 +0800

    发票改

--- src/main/webapp/js/static/js/index.js
+++ src/main/webapp/js/static/js/index.js
@@ -4,11 +4,7 @@ var projectName=pathName.substring(0,pathName.substr(1).indexOf('/')+1);
 
 if(projectName == '/captialMain' || projectName == '/tradeMain' || projectName == '/procurementMain'){
     projectName = '';
-<<<<<<< HEAD
-}else {
-=======
 }else{
->>>>>>> branch 'master' of git@code.aliyun.com:13701071207/supplyChain.git
     projectName = projectName;
 }
 $(function(){
